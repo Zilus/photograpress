ab281b4614da6d1bf2c136ebcb078136f78f0b12
Oscar Azpeitia <neozilus@gmail.com>
2017-03-14 17:45:40 -0600

    Database export and import in upgrade script

--- upgrade/upgrade.sh
+++ upgrade/upgrade.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-echo "Upgrading..."
+echo "Getting config vars"
+DBNAME=`cat ../api/config.php | grep DB_NAME | cut -d \' -f 4`
+DBUSER=`cat ../api/config.php | grep DB_USER | cut -d \' -f 4`
+DBPASSWORD=`cat ../api/config.php | grep DB_PASSWORD | cut -d \' -f 4`
+UPGRADE_PATH=`pwd`
+SQLfile=backup-$(date +\%d-\%m-\%Y_\%s).sql
+mysqldump --user=$DBUSER --password=$DBPASSWORD --host=localhost $DBNAME > $UPGRADE_PATH/sql/$SQLfile
 
 echo "GET Directus core"
 rm master.zip
@@ -20,5 +26,8 @@ rm -rfv directus-master/ master.zip
 echo "Run composer"
 cd ../
 composer install
+
+echo "importing database"
+mysql --user=$DBUSER --password=$DBPASSWORD --host=localhost $DBNAME < $UPGRADE_PATH/sql/$SQLfile
 
 echo "All done!"
